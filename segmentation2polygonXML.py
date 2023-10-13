73298d366e15f785b89326b71da8fe561a19227e
Sid <sid@adoltech.com>
2023-10-13 20:35:25 +0530

    Complete Workin' Prototype

--- segmentation2polygonXML.py
+++ segmentation2polygonXML.py
@@ -1,49 +1,58 @@
 import xml.etree.ElementTree as ET
+from colorama import Fore, Style
 from tkinter import filedialog
+from tqdm import tqdm
+import glob
 
-f = filedialog.askopenfilename(filetypes=[("XML files", "*.xml")])
+print(Fore.YELLOW+Style.BRIGHT+"\n\nSelect in-Content Directory"+Fore.RESET)
+inDir = filedialog.askdirectory()
+print(Fore.CYAN+Style.BRIGHT+"\n\nand Come On! Select outPut Directory"+Fore.RESET)
+outDir = filedialog.askdirectory() + '\\'
 
-tree = ET.parse(f)
-root = tree.getroot()
+files = glob.glob(inDir+"/*.xml")
+for f in tqdm(files, desc = "Processing Stuff!"):
+    outFile = f.split("\\")[-1]
+    outFile = outDir + outFile
+    print(outFile)
+    tree = ET.parse(f)
+    root = tree.getroot()
 
-pathElement = root.find('path')
-if pathElement is not None:
-    pathElement.text = '..//'+root.findtext('folder')+'//'+root.findtext('filename')
-sourceElement = root.find('source')
-if sourceElement is not None:
-    root.remove(sourceElement)
+    pathElement = root.find('path')
+    if pathElement is not None:
+        pathElement.text = '..//'+root.findtext('folder')+'//'+root.findtext('filename')
+    sourceElement = root.find('source')
+    if sourceElement is not None:
+        root.remove(sourceElement)
 
-objectElements = root.findall('object')
-for objectElement in objectElements:
-    pnts = []
-    for i in objectElement.iter('value'):
-        pnts.append(i.text)
+    objectElements = root.findall('object')
+    for objectElement in objectElements:
+        pnts = []
+        for i in objectElement.iter('value'):
+            pnts.append(i.text)
 
-    if len(pnts)%2==0:
-        a = [x for x in pnts[::2]]
-        b = [y for y in pnts[1::2]]
-        pnts = list(zip(a,b))
-    else:
-        pnts.pop()
-        a = [x for x in pnts[::2]]
-        b = [y for y in pnts[1::2]]
-        pnts = list(zip(a,b))
+        if len(pnts)%2==0:
+            a = [x for x in pnts[::2]]
+            b = [y for y in pnts[1::2]]
+            pnts = list(zip(a,b))
+        else:
+            pnts.pop()
+            a = [x for x in pnts[::2]]
+            b = [y for y in pnts[1::2]]
+            pnts = list(zip(a,b))
 
-    print(pnts)
+        segmentationElement = objectElement.find('segmentations')
+        objectElement.remove(segmentationElement)
 
-    segmentationElement = objectElement.find('segmentations')
-    objectElement.remove(segmentationElement)
+        polygonElement = ET.Element("polygon")
+        for val, j in enumerate(pnts, start = 1):
+            xPnt = ET.Element("x"+str(val))
+            xPnt.text = j[0]
+            yPnt = ET.Element("y"+str(val))
+            yPnt.text = j[1]
+            polygonElement.append(xPnt)
+            polygonElement.append(yPnt)
 
-    polygonElement = ET.Element("polygon")
-    for val, j in enumerate(pnts, start = 1):
-        xPnt = ET.Element("x"+str(val))
-        xPnt.text = j[0]
-        yPnt = ET.Element("y"+str(val))
-        yPnt.text = j[1]
-        polygonElement.append(xPnt)
-        polygonElement.append(yPnt)
+        objectElement.append(polygonElement)
 
-    objectElement.append(polygonElement)
-
-    xmlString = ET.tostring(objectElement, encoding='utf8').decode('utf8')
-    print(xmlString)
+        xmlString = ET.tostring(objectElement, encoding='utf8').decode('utf8')
+    tree.write(outFile)
