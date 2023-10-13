2cfe7ea85f414f561eb960f4b58ced8a438ae977
Sid <sid@adoltech.com>
2023-10-13 20:01:22 +0530

    Workin' ProtoType

---
+++ segmentation2polygonXML.py
@@ -0,0 +1,49 @@
+import xml.etree.ElementTree as ET
+from tkinter import filedialog
+
+f = filedialog.askopenfilename(filetypes=[("XML files", "*.xml")])
+
+tree = ET.parse(f)
+root = tree.getroot()
+
+pathElement = root.find('path')
+if pathElement is not None:
+    pathElement.text = '..//'+root.findtext('folder')+'//'+root.findtext('filename')
+sourceElement = root.find('source')
+if sourceElement is not None:
+    root.remove(sourceElement)
+
+objectElements = root.findall('object')
+for objectElement in objectElements:
+    pnts = []
+    for i in objectElement.iter('value'):
+        pnts.append(i.text)
+
+    if len(pnts)%2==0:
+        a = [x for x in pnts[::2]]
+        b = [y for y in pnts[1::2]]
+        pnts = list(zip(a,b))
+    else:
+        pnts.pop()
+        a = [x for x in pnts[::2]]
+        b = [y for y in pnts[1::2]]
+        pnts = list(zip(a,b))
+
+    print(pnts)
+
+    segmentationElement = objectElement.find('segmentations')
+    objectElement.remove(segmentationElement)
+
+    polygonElement = ET.Element("polygon")
+    for val, j in enumerate(pnts, start = 1):
+        xPnt = ET.Element("x"+str(val))
+        xPnt.text = j[0]
+        yPnt = ET.Element("y"+str(val))
+        yPnt.text = j[1]
+        polygonElement.append(xPnt)
+        polygonElement.append(yPnt)
+
+    objectElement.append(polygonElement)
+
+    xmlString = ET.tostring(objectElement, encoding='utf8').decode('utf8')
+    print(xmlString)
