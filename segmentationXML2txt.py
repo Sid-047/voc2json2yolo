b556cb1a4371feef500e9ee399960fe30f85408e
Sid <sid@adoltech.com>
2023-10-14 01:57:19 +0530

    Built Executable Prototype

---
+++ segmentationXML2txt.py
@@ -0,0 +1,63 @@
+import xml.etree.ElementTree as ET
+from colorama import Fore, Style
+from tkinter import filedialog
+from tqdm import tqdm
+import glob
+
+print(Fore.YELLOW+Style.BRIGHT+"\n\nSelect in-Content Directory"+Fore.RESET)
+inDir = filedialog.askdirectory()
+print(Fore.BLUE+Style.BRIGHT+"\n\nSelect the numClasses Text File Yo!"+Fore.RESET)
+inTxt = filedialog.askopenfilename(filetypes=[("NumClass TextFile", "*.txt")])
+print(Fore.CYAN+Style.BRIGHT+"\n\nand Come On! Select outPut Directory"+Fore.RESET)
+outDir = filedialog.askdirectory() + '\\'
+
+f = open(inTxt, 'r')
+clsDict = eval(f.read())
+f.close()
+
+print(clsDict)
+files = glob.glob(inDir+"/*.xml")
+
+
+for f in tqdm(files, desc = "Gettin' the Txt File out Yo!"):
+    outFile = (f.split("\\")[-1]).split('.')[0] + '.txt'
+    outFile = outDir + outFile
+    print(outFile)
+    tree = ET.parse(f)
+    root = tree.getroot()
+
+    sizeElement = root.find('size')
+    widthElement = sizeElement.find('width')
+    heightElement = sizeElement.find('height')
+    xDiv = int(widthElement.text)
+    yDiv = int(heightElement.text)
+    
+    labelData = []
+    objectElements = root.findall('object')
+    for objectElement in objectElements:
+        nameElement = objectElement.find('name')
+        clsName = nameElement.text
+        pnts = []
+        for i in objectElement.iter('value'):
+            pnts.append(i.text)
+
+        if len(pnts)%2==0:
+            a = [int(x)/xDiv for x in pnts[::2]]
+            b = [int(y)/yDiv for y in pnts[1::2]]
+            pnts = list(zip(a,b))
+            odns = [str(float(w)) for z in pnts for w in z]
+        else:
+            pnts.pop()
+            a = [int(x)/xDiv for x in pnts[::2]]
+            b = [int(y)/yDiv for y in pnts[1::2]]
+            pnts = list(zip(a,b))
+            odns = [str(float(w)) for z in pnts for w in z]
+
+        loadData = ' '.join([str(clsDict[clsName])] + odns)
+        labelData.append(loadData)
+    txtData = '\n'.join(labelData)
+    f_ = open(outFile, 'w')
+    f_.write(txtData)
+    f_.close()
+        
+        
