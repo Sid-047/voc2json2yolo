eb024f33c0e90bb425314332724bad4ca66dadce
Sid <sid@adoltech.com>
2023-10-14 21:15:57 +0530

    Updated YAML Stuff

--- XML2numClass.py
+++ XML2numClass.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 from colorama import Fore, Style
 from tkinter import filedialog
 from tqdm import tqdm
+import yaml
 import glob
 
 print(Fore.YELLOW+Style.BRIGHT+"\n\nSelect in-Content Directory"+Fore.RESET)
@@ -17,9 +18,22 @@ for f in tqdm(files, desc = "Fetching Class Names Yo!"):
         clsSet.add(i.text)
 
 clsList = list(clsSet)
+clsList.sort()
 print(clsList)
 clsNum = [x for x in range(0, len(clsList))]
 clsDict = dict(zip(clsList, clsNum))
 f = open("numClasses.txt", 'w')
 f.write(str(clsDict))
+f.close()
+
+yamlDict = dict(
+    train = "../train/images",
+    val = "../valid/images",
+    test = "../test/images",
+    nc = len(clsDict.keys()),
+    names = str(clsList)
+)
+
+f = open("data.yaml", 'w')
+yaml.dump(yamlDict, f, default_flow_style=False)
 f.close()
