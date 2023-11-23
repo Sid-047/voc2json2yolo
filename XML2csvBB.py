201d4a0893b24fc220ad5a10acfe4e63387776cc
Sid <sid@adoltech.com>
2023-11-23 23:04:19 +0530

    Initiated DataSplit Work

--- XML2csvBB.py
+++ XML2csvBB.py
@@ -11,10 +11,15 @@ inDir = filedialog.askdirectory()
 print(Fore.CYAN+Style.BRIGHT+"\n\nand Come On! Select outPut CSV Directory"+Fore.RESET)
 outDir = filedialog.askdirectory() + '\\'
 
+csvTrainData = [['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']]
+csvTestData = [['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']]
 csvData = [['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']]
 files = glob.glob(inDir+"/*.xml")
-for f in tqdm(files, desc = "Gettin' the CSV File out Yo!"):
-    outFile = outDir + "csvCompleteFile.csv"
+dataNum = len(files)
+trainNum = round(dataNum*0.8)
+for f in tqdm(files[:trainNum], desc = "Gettin' the CSV File out Yo!"):
+    outFile = (f.split("\\")[-1]).split('.')[0] + '.txt'
+    outFile = outDir + outFile
     print(outFile)
     tree = ET.parse(f)
     root = tree.getroot()
@@ -38,7 +43,38 @@ for f in tqdm(files, desc = "Gettin' the CSV File out Yo!"):
         yMax = int(float(bbxElement.find('ymax').text))
 
         labelData.extend([imgName, imgWidth, imgHeight, clsName, xMin, yMin, xMax, yMax])
+        csvTrainData.append(labelData)
         csvData.append(labelData)
+
+for f in tqdm(files[:trainNum], desc = "Gettin' the CSV File out Yo!"):
+    outFile = (f.split("\\")[-1]).split('.')[0] + '.txt'
+    outFile = outDir + outFile
+    print(outFile)
+    tree = ET.parse(f)
+    root = tree.getroot()
+
+    imgName = root.find('filename')
+    sizeElement = root.find('size')
+    widthElement = sizeElement.find('width')
+    heightElement = sizeElement.find('height')
+    imgWidth = int(widthElement.text)
+    imgHeight = int(heightElement.text)
+    
+    labelData = []
+    objectElements = root.findall('object')
+    for objectElement in objectElements:
+        nameElement = objectElement.find('name')
+        clsName = nameElement.text
+        bbxElement = objectElement.find('bndbox')
+        xMin = int(float(bbxElement.find('xmin').text))
+        yMin = int(float(bbxElement.find('ymin').text))
+        xMax = int(float(bbxElement.find('xmax').text))
+        yMax = int(float(bbxElement.find('ymax').text))
+
+        labelData.extend([imgName, imgWidth, imgHeight, clsName, xMin, yMin, xMax, yMax])
+        csvTrainData.append(labelData)
+        csvData.append(labelData)
+pprint.pprint(csvData)
 
 f = open(outFile, 'w')
 csvWriter = csv.writer(f)
