600aeaf0005c53e1edbdb79eb5ad3d17eed51fc4
Sid <sid@adoltech.com>
2023-11-25 22:26:09 +0530

    Involin' DataFrames for csvWriting

--- XML2csvBB.py
+++ XML2csvBB.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 from colorama import Fore, Style
 from tkinter import filedialog
 from tqdm import tqdm
+import pandas as pd
 import glob
 import csv
 
@@ -42,7 +43,7 @@ for f in tqdm(files[:trainNum], desc = "Gettin' the trainCSV File out Yo!"):
         csvTrainData.append(labelData)
         csvData.append(labelData)
 
-for f in tqdm(files[:trainNum], desc = "Gettin' the testCSV File out Yo!"):
+for f in tqdm(files[trainNum:], desc = "Gettin' the testCSV File out Yo!"):
     tree = ET.parse(f)
     root = tree.getroot()
 
@@ -70,22 +71,16 @@ for f in tqdm(files[:trainNum], desc = "Gettin' the testCSV File out Yo!"):
         csvData.append(labelData)
 
 outFile = outDir + 'completeLabels.csv'
-f = open(outFile, 'w')
-csvWriter = csv.writer(f)
-for row in tqdm(csvData, desc = "Writin' completeLabels Yo!", colour = "red"):
-    csvWriter.writerow(row)
-f.close()
+completeDf = pd.DataFrame(csvData)
+pd.to_csv(outFile, index=False)
+print("completeDataStuff Done Yo !")
 
 outFile = outDir + 'train_labels.csv'
-f = open(outFile, 'w')
-csvWriter = csv.writer(f)
-for row in tqdm(csvTrainData, desc = "Writin' trainLabels Yo!", colour = "red"):
-    csvWriter.writerow(row)
-f.close()
+trainDf = pd.DataFrame(csvTrainData)
+pd.to_csv(outFile, index=False)
+print("trainDataStuff Done Yo !")
 
 outFile = outDir + 'test_labels.csv'
-f = open(outFile, 'w')
-csvWriter = csv.writer(f)
-for row in tqdm(csvTestData, desc = "Writin' testLabels Yo!", colour = "red"):
-    csvWriter.writerow(row)
-f.close()
+completeDf = pd.DataFrame(csvTestData)
+pd.to_csv(outFile, index=False)
+print("testDataStuff Done Yo !")
