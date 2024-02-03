6b94db14070ff896a8c1f8cb03d737a8a01489af
Sid <sid@adoltech.com>
2024-02-03 14:56:20 +0530

    Worked on ImGDir Stuff

--- txtBB2XML.py
+++ txtBB2XML.py
@@ -3,17 +3,20 @@ from colorama import Fore, Style
 from tkinter import filedialog
 from tqdm import tqdm
 import glob
+import os
 
 print(Fore.YELLOW+Style.BRIGHT+"\n\nSelect in-TxT-Content Directory"+Fore.RESET)
-inDir = filedialog.askdirectory()
+inTxTDir = filedialog.askdirectory()
+print(Fore.YELLOW+Style.BRIGHT+"\n\nSelect LabellediMG-Content Directory"+Fore.RESET)
+inImGDir = filedialog.askdirectory()
 print(Fore.BLUE+Style.BRIGHT+"\n\nSelect the numClasses Text File Yo!"+Fore.RESET)
 inTxt = filedialog.askopenfilename(filetypes=[("NumClass TextFile", "*.txt")])
 print(Fore.CYAN+Style.BRIGHT+"\n\nand Come On! Select outPut XML Directory"+Fore.RESET)
 outDir = filedialog.askdirectory()
 if "\\" in outDir:
-    outDir = outDir + '\\'
+    slashStuff = '\\'
 else:
-    outDir = outDir + '/'
+    slashStuff = '/'
 
 f = open(inTxt, 'r')
 clsNames = f.read().replace(", ", ',').split(',')
@@ -21,20 +24,21 @@ f.close()
 clsDict = dict(zip(clsNames, list(range(len(clsNames)))))
 print(clsDict)
 
-files = glob.glob(inDir+"/*.txt")
+files = glob.glob(inTxTDir+"/*.txt")
 for f in tqdm(files, desc = "Gettin' the Txt File out Yo!"):
     f_ = f.replace("\\", "~").replace("/", "~")
     outName = (f_.split("~")[-1]).split('.')[0] + '.xml'
-    outFile = outDir + outName
+    if os.path.isfile(inImGDir + slashStuff + outName.replace(".xml", ".jpg")) or os.path.isfile(inImGDir + slashStuff + outName.replace(".xml", ".png")):
+        outFile = outDir + slashStuff + outName
+        print(outFile)
+        txtFile = open(f, 'r')
+        txtContents = txtFile.read().split('\n')
+        txtFile.close()
 
-    txtFile = open(f, 'r')
-    txtContents = txtFile.read().split('\n')
-    txtFile.close()
-
-    root = ET.Element("annotation")
-    ET.SubElement(root, "folder").text = "images"
-    ET.SubElement(root, "filename").text = outName.replace(".xml", ".jpg")
-    ET.SubElement(root, "path").text = "..//images//{}".format(outName.replace(".xml", ".jpg"))
-    
-    
-    sizeElement = ET.SubElement(root, "size")
+        root = ET.Element("annotation")
+        ET.SubElement(root, "folder").text = "images"
+        ET.SubElement(root, "filename").text = outName.replace(".xml", ".jpg")
+        ET.SubElement(root, "path").text = "..//images//{}".format(outName.replace(".xml", ".jpg"))
+        
+        
+        sizeElement = ET.SubElement(root, "size")
